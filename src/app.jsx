915bf262876edb1ef65e79684823f6e00b4f56d2
coderdan1399 <coderdan1399@gmail.com>
2018-07-01 13:07:39 +1200

    updates

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const App = () => (
+  <div>
+    <h1>hello </h1>
+    <h2> bye see ya</h2>
+    <h3> bah7</h3>
+  </div>
+)
+
+export default hot(module)(App)
---
+++ webpack.conf.dev.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+
+const path = require('path')
+const webpack = require('webpack')
+const FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin')
+module.exports = {
+  entry: './src/index.jsx',
+  mode: 'development',
+  // devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+          plugins: ['react-hot-loader/babel'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new FlowBabelWebpackPlugin(),
+  ],
+}
